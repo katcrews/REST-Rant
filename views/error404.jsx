ee64cd8dcedf3f3c0ff69067854f1cccaefaae90
Kat Crews <katfreitas808@gmail.com>
2022-07-15 14:32:22 -0700

    redo due to issues with method override

---
+++ views/error404.jsx
@@ -0,0 +1,18 @@
+const React = require('react')
+const Def = require('./default')
+
+function error404 () {
+    return (
+      <Def>
+          <main>
+              <h1>404: PAGE NOT FOUND</h1>
+              <p>Oops, sorry, we can't find this page!</p>
+              <img src="/images/cookie404.jpg" alt="Cookie is sorry he can't find your page" />
+              Photo by <a href="https://unsplash.com/@cookiethepom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Cookie the Pom</a> on <a href="https://unsplash.com/s/photos/404?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+          </main>
+      </Def>
+    )
+  }
+  
+
+module.exports = error404
---
+++ views/home.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+const Def = require('./default')
+
+function home() {
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                    <img src="/images/table.jpg" alt="Restaurant table" />
+                    <div>
+                    Photo by <a href="https://unsplash.com/es/@bartoshevicz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Adam Bartoszewicz</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = home
