4f523d1f145907a3779720050faf812253b1c941
Kat Crews <katfreitas808@gmail.com>
2022-07-23 12:43:52 -0700

    be-9 added helper method. needs better styling

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,7 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
+              <p>Oh noes!! Cookie is sorry, he can't find this page!</p>
               <img src="/images/cookie404.jpg" alt="Cookie is sorry he can't find your page" />
               Photo by <a href="https://unsplash.com/@cookiethepom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Cookie the Pom</a> on <a href="https://unsplash.com/s/photos/404?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
           </main>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,15 +5,29 @@ function show(data) {
     return (
         <Def>
             <main>
-                <div>
-                <h1>{data.place.name}</h1>
-                <img src={data.place.pic} alt={data.place.name} />
-                <h3>{data.place.city}, {data.place.state}</h3>
-                <h4> {data.place.cuisines}</h4>
-                <h2>Rating</h2>
-                <p>Not Rated</p>
-                <h2>Comments</h2>
-                <p>No comments</p>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h1>{data.place.name}</h1>
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className="col-sm-6">
+                        <h2>Description</h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                    </div>
+                    <div className="col-sm-6">
+                        <h2>Rating</h2>
+                        <p>Not Rated</p>
+                        <h2>Comments</h2>
+                        <p>No comments</p>
+                    </div>
                 </div>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
