55b61478c86f4f53fb3c8e64d7b53d79c3f408c8
Kat Crews <katfreitas808@gmail.com>
2022-07-23 13:14:28 -0700

    Be-8 pt-9 bonus-added validation error messages

--- controllers/places.js
+++ controllers/places.js
@@ -5,60 +5,67 @@ const db = require('../models')
 
 
 router.get('/', (req, res) => {
-    db.Place.find()
+  db.Place.find()
     .then((places) => {
       res.render('places/index', { places })
     })
-    .catch(err =>{
-        console.log(err);
-        res.render(error404)
+    .catch(err => {
+      console.log(err);
+      res.render(error404)
     })
 });
-
 
 router.post('/', (req, res) => {
-    db.Place.create(req.body)
+  db.Place.create(req.body)
     .then(() => {
-        res.redirect('/places')
+      res.redirect('/places')
     })
     .catch(err => {
-        console.log('err', err)
+      if (err && err.name == 'ValidationError') {
+        let message = 'Validation Error: '
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+        }
+        res.render('places/new', { message })
+      }
+      else {
         res.render('error404')
+      }
     })
 });
-  
 
 router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
-  
+  res.render('places/new')
+})
+
 router.get('/:id', (req, res) => {
-    db.Place.findById(req.params.id)
+  db.Place.findById(req.params.id)
     .then(place => {
-        res.render('places/show', { place })
+      res.render('places/show', { place })
     })
     .catch(err => {
-        console.log('err', err)
-        res.render('error404')
+      console.log('err', err)
+      res.render('error404')
     })
 })
 
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
-  })
+  res.send('PUT /places/:id stub')
+})
 
 router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id stub')
-  })
+  res.send('DELETE /places/:id stub')
+})
 
 router.get('/:id/edit', (req, res) => {
-    res.send('GET edit form stub')
-  })
+  res.send('GET edit form stub')
+})
 
 //not in previous versions
 router.post('/:id/rant', (req, res) => {
-    res.send('GET /places/:id/rant stub')
-  })
+  res.send('GET /places/:id/rant stub')
+})
 
 module.exports = router
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,11 +1,20 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form() {
+function new_form(data) {
+    let message = ''
+    if (data.message) {
+        message = (
+            <h4 className="alert-danger">
+                {data.message}
+            </h4>
+        )
+    }
     return (
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                {message}
                 <form method='POST' action='/places'>
                     <div className='form-group col-sm-4'>
                         <label htmlFor='name'>Place Name</label>
@@ -29,12 +38,12 @@ function new_form() {
                     </div>
                     <div className='form-group col-sm-4'>
                         <label htmlFor="founded">Founded Year</label>
-                        <input 
+                        <input
                             type="number"
                             className="form-control"
                             id="founded"
                             name="founded"
-                            value= {new Date().getFullYear()}/>
+                            value={new Date().getFullYear()} />
                     </div>
                     <input className='btn btn-primary' type='submit' value="Add Place" />
                 </form>
